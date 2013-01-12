86e05601f7cce8a1740a08a3fd4f8a3f0ccc9a81
Jeffrey Lutz <jeff.lutz@agileanswers.com>
2013-01-12 11:25:00 -0500

    Added first example

---
+++ hadoop-first-example/src/main/java/com/lutz/hadoop/wordcount/MyCount.java
@@ -0,0 +1,31 @@
+package com.lutz.hadoop.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+import java.io.IOException;
+
+/**
+ * User: Jeffrey M Lutz
+ * Date: 8/28/12
+ */
+public class MyCount implements Mapper<Object, Text, Text, IntWritable> {
+    @Override
+    public void map(Object o, Text text, OutputCollector<Text, IntWritable> textIntWritableOutputCollector, Reporter reporter) throws IOException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void close() throws IOException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void configure(JobConf entries) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
